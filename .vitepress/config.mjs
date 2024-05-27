5888e8a524dc3064dbe31d3073faf7de47555c36
6330A <2213518860@qq.com>
2024-05-27 12:30:08 +0800

    添加搜索框，页脚

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -13,6 +13,7 @@ export default defineConfig({
       { text: 'Home', link: '/' },
       { text: "Java", link: '/Java/Java笔记' },
       { text: "C++", link: '/C++/常用 C++ STL 用法' },
+      { text: "Help", link: '/Help/网站部署' },
     ],
 
     sidebar: {
@@ -23,14 +24,40 @@ export default defineConfig({
       '/C++': [
         { text: "常用 C++ STL 用法", link: '/C++/常用 C++ STL 用法' },
       ],
+      '/Help': [
+        { text: "网站部署", link: '/Help/网站部署' },
+      ],
     },
 
     // 隐藏目录结构，同理可以隐藏sidebar
     // sidebar: false,
     aside: false,
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
+        },
+      },
+    },
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/6330A/noteweb' }
-    ]
+    ],
+
+    footer: {
+      copyright: "Powered by <strong>vitepress</strong> | Copyrigtht © 2024.5.21-2025 | <strong>NENU</strong> License"
+    }
   }
 })
--- index.md
+++ index.md
@@ -6,20 +6,29 @@ hero:
   name: "MILILI的文档知识库"
   text: ""
   tagline: 每天记录一点点
-  actions:
-    - theme: brand
-      text: Markdown Examples
-      link: /markdown-examples
-    - theme: alt
-      text: API Examples
-      link: /api-examples
+  # actions:
+  #   - theme: brand
+  #     text: Markdown Examples
+  #     link: /markdown-examples
+  #   - theme: alt
+  #     text: API Examples
+  #     link: /api-examples
 
 features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Git commond
+    details: 
+     git init <br>
+     git clone ... <br>
+     git remote -v <br>
+
+  - title: Git commond
+    details:
+     git add . <br>
+     git commit -m "description" <br>
+     git push -u origin master <br>
+
+  - title: Git commond
+    details: 
+     git pull
 ---
 
